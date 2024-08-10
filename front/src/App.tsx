7d6451dc7c278db683e0a82251fb625270b5ee38
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-08-10 09:47:19 +0900

    add delelte function

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,5 @@
 import { Box, Button, List, ListItem, TextField } from "@mui/material";
 import "./App.css";
-// import { showTodos } from "./api/todo";
 import { useEffect, useState } from "react";
 
 type Todo = {
@@ -13,27 +12,27 @@ myHeaders.append("Content-Type", "application/json");
 export default function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
-  useEffect(() => {
-    const showTodos = async () => {
-      const url = "http://localhost:3000/todos";
-      try {
-        const response = await fetch(url, { mode: "cors" });
-        if (!response.ok) {
-          throw new Error(`resonse status ${response.status}`);
-        }
-        const todos: Todo[] = await response.json();
-        setTodos(todos);
-      } catch (error) {
-        console.log(error);
+  const getTodos = async () => {
+    const url = "http://localhost:3000/todos";
+    try {
+      const response = await fetch(url, { mode: "cors" });
+      if (!response.ok) {
+        throw new Error(`resonse status ${response.status}`);
       }
-    };
-    showTodos();
-  }, [todo]);
+      const todos: Todo[] = await response.json();
+      setTodos(todos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   const addTodo = async () => {
     const url = "http://localhost:3000/todo";
     try {
-      if (todo.length == 0){
+      if (todo.length == 0) {
         return;
       }
       const response = await fetch(url, {
@@ -45,8 +44,44 @@ export default function App() {
       if (!response.ok) {
         throw new Error(`resonse status ${response.status}`);
       }
-      const todos: Todo[] = await response.json();
-      setTodos(todos);
+      getTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const updateTodo = async (id: number) => {
+    const url = `http://localhost:3000/todo/${id}`;
+    try {
+      if (todo.length == 0) {
+        return;
+      }
+      const response = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify({ name: todo }),
+        headers: myHeaders,
+        mode: "cors",
+      });
+      if (!response.ok) {
+        throw new Error(`resonse status ${response.status}`);
+      }
+      getTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deleteTodo = async (id: number) => {
+    const url = `http://localhost:3000/todo/${id}`;
+    try {
+      const response = await fetch(url, {
+        method: "DELETE",
+        body: JSON.stringify({ id: id }),
+        headers: myHeaders,
+        mode: "cors",
+      });
+      if (!response.ok) {
+        throw new Error(`resonse status ${response.status}`);
+      }
+      getTodos();
     } catch (error) {
       console.log(error);
     }
@@ -56,7 +91,7 @@ export default function App() {
     <>
       <List>
         <h4>Todo一覧</h4>
-        {todos ? (
+        {todos && todos.length > 0 ? (
           <Box>
             {todos.map((todo) => {
               return (
@@ -69,10 +104,20 @@ export default function App() {
                       padding: "4px",
                     }}
                   >
-                    <p>{todo.name}</p>
+                      <p>{todo.name}</p>
                   </Box>
-                  <Button variant="outlined">編集</Button>
-                  <Button variant="outlined">削除</Button>
+                  <Button
+                    variant="outlined"
+                    onClick={() => updateTodo(todo.id)}
+                  >
+                    編集
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={() => deleteTodo(todo.id)}
+                  >
+                    削除
+                  </Button>
                 </ListItem>
               );
             })}
@@ -82,12 +127,14 @@ export default function App() {
         )}
       </List>
       <form>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <TextField value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <TextField
+            value={todo}
+            onChange={(e) => {
+              e.preventDefault();
+              setTodo(e.target.value);
+            }}
+          />
           <Button type="submit" onClick={addTodo} variant="contained">
             追加
           </Button>
